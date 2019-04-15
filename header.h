b93631a2d1a3c9a90d07839bc92de55bd62a6d62
Ron Wolf <ron-wolf@users.noreply.github.com>
2019-04-15 17:08:52 -0400

    Include license statement
    Add a license statement to the top of the two C files thus far, header.h
    and WTF.c

--- header.h
+++ header.h
@@ -1,5 +1,24 @@
 #ifndef HEADER__H__
 #define HEADER__H__
+// WTF - A basic client/server VC written in C.
+// Copyright (C) 2019  Tom Fiorilla and Ron Wolf
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+#ifndef __HEADER_H__
+#define __HEADER_H__
 
 #include <string.h>
 #include <stdio.h>
