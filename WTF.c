157aa9e955a461045779b33bc67516d19103b2fc
Thomas Fiorilla <engineertf@gmail.com>
2019-04-15 16:18:37 -0400

    Starting Header, update WTF.c
    a header.h with inclusions, WTF.c updates

--- WTF.c
+++ WTF.c
@@ -1,7 +1,9 @@
+#include "header.h"
+
 int main(int argc, char const *argv[])
 {
 	
 	
-
+	
 	return 0;
 }
---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER__H__
+#define HEADER__H__
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <dirent.h>
+
+#endif
