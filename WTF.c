e962135c97ea4de83eca03d63133cd419aa85746
Ron Wolf <ron-wolf@users.noreply.github.com>
2019-05-04 17:53:37 -0400

    Remove licensing header
    Devote top of actual "*.c" file to header imports

--- WTF.c
+++ WTF.c
@@ -1,18 +1,13 @@
-// WTF - A basic client/server VC written in C.
-// Copyright (C) 2019  Tom Fiorilla and Ron Wolf
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-// 
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include <dirent.h>
+#include <fcntl.h>    // O_*
+#include <string.h>
+#include <stdio.h>    // popen(), pclose()
+#include <stdlib.h>   // system()
+#include <sysexits.h> // EX_*
+#include <unistd.h>
+#include <sys/errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "datatypes.h"
 #include "macros.h"
